fafdac7d52d1fac75b3bebf2814b39b5b79c7fc4
LloydMatare <lloydsmatare@gmail.com>
2026-03-26 10:58:30 +0200

    final build

--- next.config.ts
+++ next.config.ts
@@ -9,11 +9,6 @@ const nextConfig: NextConfig = {
             },
         ],
     },
-    api: {
-        bodyParser: {
-            sizeLimit: "60mb",
-        },
-    },
 };
 
 export default nextConfig;
